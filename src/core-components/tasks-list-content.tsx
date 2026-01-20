5913e613c3182780553ad7f95d0bc848b63b6906
juniorbonini <juniorboninia7x@gmail.com>
2026-01-20 15:44:32 -0300

    Aplicando regras de negócio - Preparar tarefa

--- src/core-components/tasks-list-content.tsx
+++ src/core-components/tasks-list-content.tsx
@@ -6,12 +6,16 @@ import TasksContent from "./tasks-content";
 import useTasks from "../hooks/useTasks";
 
 export default function TasksListContent() {
-  const { tasks } = useTasks();
+  const { tasks, prepareTasks } = useTasks();
+
+  function handleNewTask() {
+    prepareTasks()
+  }
 
   console.log(tasks);
   return (
     <>
-      <Button icon={PlusIcon} className="w-full">
+      <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
         Nova tarefa
       </Button>
       <section className="space-y-2">
--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,13 +1,24 @@
-
 import { useLocalStorage } from "usehooks-ts";
-import { TASKS_KEY, type Task } from "../types/task";
+import { TASKS_KEY, TaskState, type Task } from "../types/task";
 
 export default function useTasks() {
   const [tasks, setTasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
+
+  function prepareTasks() {
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString(36).substring(2, 9),
+        title: "",
+        state: TaskState.Creating,
+      },
+    ]);
+  }
 
   return {
     tasks,
     tasksCount: tasks.length,
     completedTasksCount: tasks.filter((task) => task.isCompleted).length,
+    prepareTasks,
   };
 }
